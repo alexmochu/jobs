dc9fe266be5e169238d5d84add2fa488201bdb8c
alexmochu <mochualex4@gmail.com>
2023-04-05 19:04:49 +0300

    move token_required to utilities

--- api/auth/views.py
+++ api/auth/views.py
@@ -17,6 +17,7 @@ from functools import wraps
 from . import auth
 from .. import db
 from ..models import OAuth, User, BlacklistToken
+from ..utilities import token_required
 
 github_blueprint = make_github_blueprint(
     client_id=os.getenv("GITHUB_ID"),
@@ -115,35 +116,6 @@ def linkedin_logged_in(blueprint, token):
             db.session.add(user)
             db.session.commit()
         login_user(user)
-        
-# authorize and authenticate with jwt token
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        access_token = request.headers['header-access-token']
-        blacklisted = BlacklistToken.query.filter_by(token=access_token).first()
-
-        if blacklisted:
-            response = {"message": "Logged out. Please login again!" }
-            return make_response(jsonify(response)), 401
-
-        if 'header-access-token' in request.headers:
-            token = request.headers['header-access-token']
-
-        if not token:
-            return jsonify({'message': 'Token is missing!'})
-
-        try:
-            data = jwt.decode(token, current_app.config.get('SECRET_KEY'), algorithms=["HS256"])
-            current_user_data = User.query.filter_by(username=data['username']).first()
-        except jwt.ExpiredSignatureError:
-            return jsonify({'message': 'Token expired, log in again'}), 403
-        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Invalid token. Please log in again.'}), 403
-        return f(current_user_data, data, *args, **kwargs)
-
-    return decorated
  
 # @auth.route("/github")
 # def github_login():
