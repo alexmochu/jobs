4f209f41016a73b8b717323aee1e7bee87fcdef4
alexmochu <mochualex4@gmail.com>
2023-04-05 20:43:41 +0300

    job application question prompt

--- api/gpt/views.py
+++ api/gpt/views.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask, request
-from functools import wraps
+from flask import request, redirect, url_for
+
 import openai
 
 from . import gpt
@@ -9,43 +9,33 @@ from ..utilities import token_required
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
   
-@gpt.route('/kejani-ai/openai', methods=['GET', 'POST'])
+@gpt.route('/kejani-ai/question', methods=['GET', 'POST'])
 @token_required
-def openai(current_user_data, user_id):
+def question_gpt(current_user_data, user_id):
     if request.method == "POST":
-        animal = request.form["animal"]
+        question = request.form["question"]
+        job = request.form["job"]
+        purpose = request.form["purpose"]
+        position = request.form["position"]
+        accountabilities = request.form["accountabilities"]
+        resume = request.form["resume"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=question_answer_propmt(question, job, purpose, position, accountabilities, resume),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
-    return {'ai': 'this openai endpoint'}, 200
+    return result, 200
 
-# @app.route("/", methods=("GET", "POST"))
-# def index():
-#     if request.method == "POST":
-#         animal = request.form["animal"]
-#         response = openai.Completion.create(
-#             model="text-davinci-003",
-#             prompt=generate_prompt(animal),
-#             temperature=0.6,
-#         )
-#         return redirect(url_for("index", result=response.choices[0].text))
-
-#     result = request.args.get("result")
-#     return render_template("index.html", result=result)
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def question_answer_propmt(question, job, purpose, position, accountabilities, resume):
+    return """Write not more than a 300-word answer to a targeted question for job application
+Targeted question= {question}
+Base the answer using position {purpose} key accountabilities and resume
+Position purpose = {purpose}
+Key accountabilities = {accountabilities}
+Resume = {resume}
+Use key words from key accountabilities in the answer
+Key accountabilities = {accountabilities}
+"""
